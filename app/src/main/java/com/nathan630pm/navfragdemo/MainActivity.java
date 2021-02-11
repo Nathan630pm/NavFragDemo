d1d0664e9b14b455b145a63d3eef595993de864a
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 18:55:21 -0500

    initial commit

---
+++ app/src/main/java/com/nathan630pm/navfragdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nathan630pm.navfragdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
