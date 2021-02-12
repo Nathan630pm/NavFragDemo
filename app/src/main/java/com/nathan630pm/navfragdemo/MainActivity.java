54c070b7519db099f594c30501fe4c75d3eba6b2
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 20:42:19 -0500

    i did more stuff

--- app/src/main/java/com/nathan630pm/navfragdemo/MainActivity.java
+++ app/src/main/java/com/nathan630pm/navfragdemo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.nathan630pm.navfragdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavHostFragment navHostFragment;
+    private NavController nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        nav = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, nav);
+
     }
 }
