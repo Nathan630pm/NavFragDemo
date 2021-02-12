9fedc273697327c8c1f829d3949e5598457d0362
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 21:53:11 -0500

    added a drawer lul

--- app/src/main/java/com/nathan630pm/navfragdemo/MainActivity.java
+++ app/src/main/java/com/nathan630pm/navfragdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nathan630pm.navfragdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -8,8 +10,11 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private NavController nav;
 
     private AppBarConfiguration appBarConfiguration;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
 
         nav = Navigation.findNavController(this, R.id.fragment);
 
-        appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.profileFragment, R.id.settingsFragment).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.profileFragment, R.id.settingsFragment).setDrawerLayout(drawerLayout).build();
+
+        NavigationUI.setupWithNavController(navigationView, nav);
         NavigationUI.setupWithNavController(bottomNavigationView, nav);
 
         NavigationUI.setupActionBarWithNavController(this, nav, appBarConfiguration);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() != R.id.placeholderFragment) {
+//            drawerLayout.openDrawer(drawerLayout);
+        }
+        return NavigationUI.onNavDestinationSelected(item, nav) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        return super.onNavigateUp();
     }
 }
--- app/src/main/java/com/nathan630pm/navfragdemo/fragments/PlaceholderFragment.java
+++ app/src/main/java/com/nathan630pm/navfragdemo/fragments/PlaceholderFragment.java
@@ -9,13 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.nathan630pm.navfragdemo.R;
 
 
-public class PlaceholderFragment  extends Fragment {
+public class PlaceholderFragment  extends BottomSheetDialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_placeholder, container, false);
     }
 }
